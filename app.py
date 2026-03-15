3049d496d25537e12c9b4f12a861f4bc9420f371
Aman <amanshaikhn2020@gmail.com>
2026-03-15 14:46:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Cloud Auto Deployment Working! Version 2"
+    return "Hieeeee guyesssss"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
